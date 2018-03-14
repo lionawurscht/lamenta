ec2c071ddb99a143de0dded598eebcb1366aa025
Lion <extra.lion.w@gmail.com>
2018-03-14 09:21:59 +0100

    Added doit

--- dodo.py
+++ dodo.py
@@ -1,10 +1,11 @@
 #!/usr/bin/env python
 from pathlib import Path
 import sass
+import os
 
-working_directory = Path('.')
-scss_directory = working_directory / 'scss'
-print(working_directory)
+package_directory = Path(os.path.dirname(os.path.abspath(__file__)))
+package_name = package_directory.name
+scss_directory = package_directory / 'scss'
 
 def task_css():
     dependencies = (list(scss_directory.glob('**/*.scss'))
@@ -16,10 +17,13 @@ def task_css():
             output_style='compressed'
         )
 
+    target = package_directory / 'lamenta.css'
+
     return {
-        'actions': [(compile_sass, (scss_directory, working_directory))],
+        'basename': package_name + ':css',
+        'actions': [(compile_sass, (scss_directory, package_directory))],
         'file_dep': dependencies,
-        'targets': [working_directory / 'lamenta.css'],
+        'targets': [target],
         'verbosity': 2,
     }
 
